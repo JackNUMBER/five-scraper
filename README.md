2b7b8340ac0479537a83fe3de12fa429ca8d7364
JackNUMBER <antoine.cdrt@gmail.com>
2022-03-31 22:57:27 +0200

    initial commit \o/

---
+++ README.md
@@ -0,0 +1,96 @@
+The purpose of this project is to facilitate the booking of futsal "soccer five" game slots.\
+Currently only LE FIVE (https://lefive.fr) centers are available.
+
+## Install and run
+
+Requirement: Node.js + npm
+
+1. Clone the repos.
+2. Run `npm i`
+3. Run `npm run get-slot`
+
+## Custom
+
+The script works by time iteration, meaning it will gives you each slots available in the date and time range you have setted.\
+You can the number of weeks you want to check (starting today), wich day you want to play, at what time and where (centers).
+
+At the moment game duration is not customisable (set to 60 min) but you can set it in the request.
+
+### Weeks
+
+Edit `WEEKS_FROM_NOW` to set until when you want you want to check slots.\
+The script start from today (the day when you run the script).
+
+> Eg. `WEEKS_FROM_NOW = 4` will request slots for the next 4 weeks.
+
+### Week days
+
+Edit `WEEK_DAYS` to set wich days of the week you want to play.
+
+> Eg. `WEEK_DAYS = [1, 2, 6]` will request slots for Monday, Tuesday and Saturday.
+
+### Times
+
+Edit `TIME_START` and `TIME_END` (24-hour) to set your wanted time range.\
+The script will check every 30 minutes slots between this two times.
+
+> Eg. `TIME_START = '19:00'` and `TIME_END = '20:30'` will request 3 slots (19:00, 19:30, 20:00).
+
+### Centers
+
+To add center you can take somes in the list bellow and add them in the `centers` const.
+
+> Eg. centers = [ LE_FIVE_VILETTE, LE_FIVE_PARIS_18 ]` will search in the 2 centers.
+
+#### List of centers: (up-to-date on 31/03/2022)
+
+```javascript
+// Île-de-France
+const LE_FIVE_BEZON = 18;
+const LE_FIVE_BOBIGNY = 19;
+const LE_FIVE_CARRIERES = 4;
+const LE_FIVE_CHAMPIGNY = 5;
+const LE_FIVE_CRETEIL = 25;
+const LE_FIVE_MARVILLE = 25;
+const LE_FIVE_MORANGIS = 59;
+const LE_FIVE_PARIS_13 = 51;
+const LE_FIVE_PARIS_17 = 63;
+const LE_FIVE_PARIS_18 = 69;
+const LE_FIVE_SARCELLES = 35;
+const LE_FIVE_SARCELLES = 35;
+const LE_FIVE_VILETTE = 39;
+
+// Nord
+const LE_FIVE_DUNKERQUE = 49;
+const LE_FIVE_LENS = 41;
+const LE_FIVE_ROUEN = 12;
+const LE_FIVE_VALENCIENNES = 52;
+
+// Grand-Est
+const LE_FIVE_METZ = 53;
+const LE_FIVE_MULHOUSE = 50;
+const LE_FIVE_NANCY = 43;
+const LE_FIVE_REIMS = 47;
+const LE_FIVE_SAINT_LOUIS_BALE = 61;
+const SOCCER_PARK_STRASBOURG = 10;
+
+// Centre
+const LE_FIVE_ORLEANS_FLEURY = 9;
+const LE_FIVE_ORLEANS_INGRE = 31;
+
+// Rhône-Alpes PACA
+const LE_FIVE_ANNEMASSE = 16;
+const LE_FIVE_ANTIBES = 1;
+const LE_FIVE_LYON = 71;
+const LE_FIVE_VITROLLES = 40;
+
+// Nouvelle-Aquitaine & Occitanie
+const LE_FIVE_BORDEAUX = 2;
+const LE_FIVE_LA_ROCHELLE = 42;
+const LE_FIVE_LIMOGES = 48;
+const LE_FIVE_PAU = 7;
+const LE_FIVE_PERPIGNAN = 33;
+
+// Réunion
+const LE_FIVE_SAINT_LOUIS_LA_REUNION = 52;
+```
---
+++ getSlots.js
@@ -0,0 +1,162 @@
+const axios = require('axios').default;
+const {
+  add,
+  format,
+  getISODay,
+  getISOWeek,
+  isWithinInterval,
+  set: fnsSet,
+} = require('date-fns');
+const frenchLocale = require('date-fns/locale/fr');
+
+const LE_FIVE_VILETTE = 39;
+const LE_FIVE_PARIS_18 = 69;
+const LE_FIVE_PARIS_17 = 63;
+const LE_FIVE_PARIS_13 = 51;
+
+const centers = [
+  LE_FIVE_VILETTE,
+  LE_FIVE_PARIS_18,
+  // LE_FIVE_PARIS_17,
+  // LE_FIVE_PARIS_13,
+];
+
+const WEEKS_FROM_NOW = 4; // number of weeks next today we will search a slot
+
+const WEEK_DAYS = [1, 2, 3]; // list of week days (eg. monday = 1)
+
+const TIME_START = '19:00';
+const TIME_END = '21:00';
+
+const DELAY_BETWEEN_REQUEST = 100;
+
+const slots = [];
+
+const today = new Date();
+let currentDate = today;
+const currentWeek = getISOWeek(currentDate);
+const lastDate = add(currentDate, { weeks: WEEKS_FROM_NOW });
+const lastWeek = getISOWeek(lastDate);
+
+const timeStartHours = TIME_START.split(':')[0];
+const timeStartMinutes = TIME_START.split(':')[1];
+const timeEndtHours = TIME_END.split(':')[0];
+const timeEndtMinutes = TIME_END.split(':')[1];
+
+console.log('today:', today, 'week:', currentWeek);
+console.log('last day: ', lastDate, 'week:', lastWeek);
+
+let countWeek = 0;
+let previousWeek = currentWeek;
+let countDay = 0;
+let countWantedDay = 0;
+
+const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+
+// loop for days
+async function scrap() {
+  while (countWeek <= WEEKS_FROM_NOW) {
+    if (WEEK_DAYS.includes(getISODay(currentDate))) {
+      console.log(
+        'week:',
+        getISOWeek(currentDate),
+        'weekday:',
+        getISODay(currentDate),
+      );
+
+      // reset hours
+      currentDate = fnsSet(currentDate, {
+        hours: timeStartHours,
+        minutes: timeStartMinutes,
+        seconds: 0,
+        milliseconds: 0,
+      });
+
+      // loop for hours
+      // continue if the current hour is not between the defined time interval
+      while (
+        isWithinInterval(currentDate, {
+          start: fnsSet(currentDate, {
+            hours: timeStartHours,
+            minutes: timeStartMinutes,
+          }),
+          end: fnsSet(currentDate, {
+            hours: timeEndtHours,
+            minutes: timeEndtMinutes,
+          }),
+        })
+      ) {
+        console.log(currentDate);
+
+        centers.forEach((center) => {
+          slots.push(getSlotData(currentDate, center));
+        });
+
+        await timer(DELAY_BETWEEN_REQUEST); // avoid 502 on server
+
+        // go to next slot
+        currentDate = add(currentDate, { minutes: 30 });
+      }
+      countWantedDay++;
+    }
+
+    // go to next day
+    currentDate = add(currentDate, { days: 1 });
+    countDay++;
+
+    // increment week
+    if (previousWeek !== getISOWeek(currentDate)) {
+      countWeek++;
+      previousWeek = getISOWeek(currentDate);
+    }
+  }
+
+  function getSlotData(date, center) {
+    const datePlusOneHour = add(date, { hours: 1 });
+
+    return axios
+      .post(
+        'https://api.lefive.fr/splf/v1/bookingrules/availableFields/front?appId=1&isChannelWeb=true',
+        {
+          startingDateZuluTime: date,
+          duration: 60,
+          center_id: center,
+          bookingType_id: '1',
+          sportType_id: '1',
+          isChannelWeb: true,
+          capacity: '10',
+          endingDateZuluTime: datePlusOneHour,
+        },
+      )
+      .then(function (response) {
+        return response.data;
+      })
+      .catch(function (error) {
+        console.log(
+          `[${error.response.status}] ${error.response.statusText} | [${error.response.data.code}] ${error.response.data.description}`,
+        );
+      });
+  }
+
+  function formatResponse(response) {
+    if (!response[0]) return;
+
+    const formatedDate = format(
+      new Date(response[0]['startingDateZuluTime']),
+      'iiii dd LLLL - kk:mm',
+      { locale: frenchLocale },
+    );
+    const countSlots = response.length;
+    const price = `${response[0]['price']}€`;
+    const centerName = response[0]['center']['centerName'];
+
+    return `${formatedDate} (${countSlots} slots, ${price}) @ ${centerName}`;
+  }
+
+  Promise.all(slots).then(function (promises) {
+    promises = promises.filter((x) => x.length > 0);
+    console.log(promises.map((item) => formatResponse(item)));
+  });
+}
+
+scrap();
